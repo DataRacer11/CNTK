60536ed96a3c4db0fd06ed05c71f98081c13c9c6
KeDengMS <kedeng@microsoft.com>
2017-01-12 16:03:37 -0800

    Use fixture in test

--- Tests/UnitTests/MathTests/MatrixLearnerTests.cpp
+++ Tests/UnitTests/MathTests/MatrixLearnerTests.cpp
@@ -12,58 +12,59 @@
 
 using namespace Microsoft::MSR::CNTK;
 
-static const size_t dim1 = 256;
-static const size_t dim2 = 128;
-static const size_t dim3 = 2048;
-
-static void PrepareMatrices(
-    SingleMatrix& matSG,
-    SingleMatrix& matSGsparse,
-    SingleMatrix& matM,
-    SingleMatrix& matMsparse,
-    SingleMatrix& matG,
-    SingleMatrix& matGsparseBSC,
-    RandomSeedFixture& r)
+class MatrixLearnerFixture : public RandomSeedFixture
 {
-    // smoothed gradient
-    matSG = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, r.IncrementCounter());
-    matSGsparse = SingleMatrix(matSG.DeepClone());
+public:
+    static const size_t dim1 = 256;
+    static const size_t dim2 = 128;
+    static const size_t dim3 = 2048;
 
-    // model
-    matM = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, r.IncrementCounter());
-    matMsparse = SingleMatrix(matM.DeepClone());
+    SingleMatrix matSG;
+    SingleMatrix matSGsparse;
+    SingleMatrix matM;
+    SingleMatrix matMsparse;
+    SingleMatrix matG;
+    SingleMatrix matGsparseBSC;
 
-    // generates gradient
-    SingleMatrix matG1(c_deviceIdZero);
-    matG1.AssignTruncateBottomOf(Matrix<float>::RandomUniform(dim2, dim3, c_deviceIdZero, -300.0f, 0.1f, r.IncrementCounter()), 0);
+    MatrixLearnerFixture() :
+        matSG(c_deviceIdZero),
+        matSGsparse(c_deviceIdZero),
+        matM(c_deviceIdZero),
+        matMsparse(c_deviceIdZero),
+        matG(c_deviceIdZero),
+        matGsparseBSC(c_deviceIdZero)
+    {
+        // smoothed gradient
+        matSG = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
+        matSGsparse = SingleMatrix(matSG.DeepClone());
 
-    SingleMatrix matG1sparseCSC(matG1.DeepClone());
-    matG1sparseCSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSC, true);
+        // model
+        matM = SingleMatrix::RandomGaussian(dim1, dim2, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
+        matMsparse = SingleMatrix(matM.DeepClone());
 
-    SingleMatrix matG2 = SingleMatrix::RandomGaussian(dim1, dim3, c_deviceIdZero, -1.0f, 1.0f, r.IncrementCounter());
+        // generates gradient
+        SingleMatrix matG1(c_deviceIdZero);
+        matG1.AssignTruncateBottomOf(Matrix<float>::RandomUniform(dim2, dim3, c_deviceIdZero, -300.0f, 0.1f, IncrementCounter()), 0);
 
-    SingleMatrix::MultiplyAndWeightedAdd(1, matG2, false, matG1, true, 0, matG);
+        SingleMatrix matG1sparseCSC(matG1.DeepClone());
+        matG1sparseCSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSC, true);
 
-    matGsparseBSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseBlockCol, false);
-    SingleMatrix::MultiplyAndAdd(matG2, false, matG1sparseCSC, true, matGsparseBSC);
-}
+        SingleMatrix matG2 = SingleMatrix::RandomGaussian(dim1, dim3, c_deviceIdZero, -1.0f, 1.0f, IncrementCounter());
+
+        SingleMatrix::MultiplyAndWeightedAdd(1, matG2, false, matG1, true, 0, matG);
+
+        matGsparseBSC.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseBlockCol, false);
+        SingleMatrix::MultiplyAndAdd(matG2, false, matG1sparseCSC, true, matGsparseBSC);
+    }
+};
 
 namespace Microsoft { namespace MSR { namespace CNTK { namespace Test {
 
 BOOST_AUTO_TEST_SUITE(MatrixLearnerSuite)
 
 // tests FSAdagrad sparse vs. dense
-BOOST_FIXTURE_TEST_CASE(FSAdagradSparse, RandomSeedFixture)
+BOOST_FIXTURE_TEST_CASE(FSAdagradSparse, MatrixLearnerFixture)
 {
-    SingleMatrix matSG(c_deviceIdZero);
-    SingleMatrix matSGsparse(c_deviceIdZero);
-    SingleMatrix matM(c_deviceIdZero);
-    SingleMatrix matMsparse(c_deviceIdZero);
-    SingleMatrix matG(c_deviceIdZero);
-    SingleMatrix matGsparseBSC(c_deviceIdZero);
-
-    PrepareMatrices(matSG, matSGsparse, matM, matMsparse, matG, matGsparseBSC, *this);
-
     // run learner
     double smoothedCount = 1000;
     matSG.FSAdagradUpdate(dim2, matG, matM, smoothedCount, 0.0001, 1.0, 0.9, 0.9);
@@ -76,17 +77,8 @@ BOOST_FIXTURE_TEST_CASE(FSAdagradSparse, RandomSeedFixture)
 }
 
 // tests RmsProp sparse vs. dense
-BOOST_FIXTURE_TEST_CASE(RmsPropSparse, RandomSeedFixture)
+BOOST_FIXTURE_TEST_CASE(RmsPropSparse, MatrixLearnerFixture)
 {
-    SingleMatrix matSG(c_deviceIdZero);
-    SingleMatrix matSGsparse(c_deviceIdZero);
-    SingleMatrix matM(c_deviceIdZero);
-    SingleMatrix matMsparse(c_deviceIdZero);
-    SingleMatrix matG(c_deviceIdZero);
-    SingleMatrix matGsparseBSC(c_deviceIdZero);
-
-    PrepareMatrices(matSG, matSGsparse, matM, matMsparse, matG, matGsparseBSC, *this);
-
     // run learner
     matSG.RmsProp(matG, 0.99f, 1.2f, 10.0f, 0.75f, 0.1f, false);
     matSGsparse.RmsProp(matGsparseBSC, 0.99f, 1.2f, 10.0f, 0.75f, 0.1f, false);
